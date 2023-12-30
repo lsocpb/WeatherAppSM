01f08a7fd58e21e55521246dd2cd84ff3881c3ed
lsocpb <lukasssonik@gmail.com>
2023-12-31 00:18:51 +0100

    first layout setup

--- app/src/main/java/com/example/weatherappsm/MainActivity.java
+++ app/src/main/java/com/example/weatherappsm/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.weatherappsm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView idTVtemp, idTVcityName;
+    private ImageView idIVHomebg;
+
+    private RelativeLayout idRLhome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        idTVcityName = findViewById(R.id.idTVcityName);
+        idTVtemp = findViewById(R.id.idTVtemp);
+        idIVHomebg = findViewById(R.id.idIVHomebg);
+        idRLhome = findViewById(R.id.idRLHome);
     }
+
 }
