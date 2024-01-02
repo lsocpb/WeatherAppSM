a1b7001bffd26f9266238b43f66dc9cd92f3016a
lsocpb <lukasssonik@gmail.com>
2024-01-03 00:43:18 +0100

    background load in case if its day or night

--- app/src/main/java/com/example/weatherappsm/MainActivity.java
+++ app/src/main/java/com/example/weatherappsm/MainActivity.java
@@ -178,6 +178,13 @@ public class MainActivity extends AppCompatActivity {
                     WeatherResponse weatherResponse = response.body();
                     if (weatherResponse != null) {
                         String temperature = weatherResponse.getCurrent().getTemperature();
+                        int isDay = weatherResponse.getCurrent().getIsDay();
+                        if(isDay == 0){
+                            Picasso.get().load("https://images.unsplash.com/photo-1615233262415-1a942703bc71?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").into(idIVHomebg);
+                        }
+                        else{
+                            Picasso.get().load("https://images.unsplash.com/photo-1617150119111-09bbb85178b0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D").into(idIVHomebg);
+                        }
                         idTVtemp.setText(temperature + "°C");
                         idTVcityName.setText(cityName);
                     }
