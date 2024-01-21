76b8a0a1eb441bf93a697f9894468207c2cd19a8
hsup <kontonastatystyke@gmail.com>
2024-01-21 17:56:19 +0100

    Extra snackbars in MeasurementsActivity

--- app/src/main/java/com/example/weatherappsm/activities/settings/SettingsMeasurementsActivity.java
+++ app/src/main/java/com/example/weatherappsm/activities/settings/SettingsMeasurementsActivity.java
@@ -1,6 +1,7 @@
 package com.example.weatherappsm.activities.settings;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.RadioButton;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import com.example.weatherappsm.R;
 import com.example.weatherappsm.db.model.Settings;
 import com.example.weatherappsm.db.model.User;
 import com.example.weatherappsm.manager.UserManager;
+import com.google.android.material.snackbar.Snackbar;
 
 public class SettingsMeasurementsActivity extends AppCompatActivity {
 
@@ -47,17 +49,47 @@ public class SettingsMeasurementsActivity extends AppCompatActivity {
 
 
 
-        idBtnC.setOnClickListener(v -> settings.setTemperatureUnit(Settings.TemperatureUnit.CELSIUS));
-        idBtnF.setOnClickListener(v -> settings.setTemperatureUnit(Settings.TemperatureUnit.FAHRENHEIT));
+        idBtnC.setOnClickListener(v -> {
+            settings.setTemperatureUnit(Settings.TemperatureUnit.CELSIUS);
+            showSnackbar(getString(R.string.temperature_unit_changed) + " " + settings.getTemperatureUnit());
+        });
+        idBtnF.setOnClickListener(v -> {
+            settings.setTemperatureUnit(Settings.TemperatureUnit.FAHRENHEIT);
+            showSnackbar(getString(R.string.temperature_unit_changed) + " " + settings.getTemperatureUnit());
+        });
 
-        idBtn24h.setOnClickListener(v -> settings.setHourFormat(Settings.HourFormat.TWENTY_FOUR));
-        idBtn12h.setOnClickListener(v -> settings.setHourFormat(Settings.HourFormat.TWELVE));
+        idBtn24h.setOnClickListener(v -> {
+            settings.setHourFormat(Settings.HourFormat.TWENTY_FOUR);
+            showSnackbar(getString(R.string.hour_format_changed) + " " + settings.getHourFormat());
+        });
+        idBtn12h.setOnClickListener(v -> {
+            settings.setHourFormat(Settings.HourFormat.TWELVE);
+            showSnackbar(getString(R.string.hour_format_changed) + " " + settings.getHourFormat());
+        });
 
-        idBtnKMH.setOnClickListener(v -> settings.setWindSpeedUnit(Settings.WindSpeedUnit.KILLOMETERS_PER_HOUR));
-        idBtnMPH.setOnClickListener(v -> settings.setWindSpeedUnit(Settings.WindSpeedUnit.MILES_PER_HOUR));
+        idBtnKMH.setOnClickListener(v -> {
+            settings.setWindSpeedUnit(Settings.WindSpeedUnit.KILLOMETERS_PER_HOUR);
+            showSnackbar(getString(R.string.wind_speed_unit_changed) + " " + settings.getWindSpeedUnit());
+        });
+        idBtnMPH.setOnClickListener(v -> {
+            settings.setWindSpeedUnit(Settings.WindSpeedUnit.MILES_PER_HOUR);
+            showSnackbar(getString(R.string.wind_speed_unit_changed) + " " + settings.getWindSpeedUnit());
+        });
 
-        idBtnHPA.setOnClickListener(v -> System.out.println("Hectopascals button clicked"));
-        idBtnMMHG.setOnClickListener(v -> System.out.println("Millimeters of mercury button clicked"));
+        idBtnHPA.setOnClickListener(v -> {
+            System.out.println("Hectopascals button clicked");
+            //showSnackbar(getString(R.string.pressure_unit_changed) + " " + settings.getPressureUnit());
+        });
+        idBtnMMHG.setOnClickListener(v -> {
+            System.out.println("Millimeters of mercury button clicked");
+            //showSnackbar(getString(R.string.pressure_unit_changed) + " " + settings.getPressureUnit());
+        });
+    }
+
+    private void showSnackbar(String message) {
+        View view = findViewById(android.R.id.content);
+        Snackbar snackbar = Snackbar.make(view, message, Snackbar.LENGTH_SHORT);
+        snackbar.show();
     }
 }
 
